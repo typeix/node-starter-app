10c4cac055c032e2185ac0a420c69a626f9e8089
igorzg <igor.ivanovic@pm.me>
2021-05-15 23:37:18 +0200

    Added custom logger

--- api-typeorm-sql/src/modules/datastore/configs/pg.config.ts
+++ api-typeorm-sql/src/modules/datastore/configs/pg.config.ts
@@ -1,16 +1,24 @@
 import {CreateProvider, Injectable} from "@typeix/resty";
-import {Connection, createConnection} from "typeorm";
+import {Connection, createConnection, Logger} from "typeorm";
 import {Repository} from "typeorm/repository/Repository";
 import {EntityTarget} from "typeorm/common/EntityTarget";
+import * as pgConfig from "./ormconfig.json";
+import {ConnectionOptions} from "typeorm/connection/ConnectionOptions";
+import {PgLoggerConfig} from "~/modules/datastore/configs/pg.logger.config";
 
 @Injectable()
 export class PgConfig {
 
   @CreateProvider({
     provide: Connection,
-    useFactory: async () => {
-      return await createConnection()
-    }
+    useFactory: async (logger: Logger) => {
+      return await createConnection({
+        ...<ConnectionOptions>pgConfig,
+        logger
+      })
+
+    },
+    providers: [PgLoggerConfig]
   }) private connection: Connection;
 
   getRepository<T>(entity: EntityTarget<T>): Repository<T> {
